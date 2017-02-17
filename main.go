bf8f3a0b87bcec5afc2ef5fdaa8d55e369488139
Christian Grabowski <christiang11754@gmail.com>
2017-02-16 19:43:15 -0500

    add flannel flags

--- main.go
+++ main.go
@@ -18,6 +18,22 @@ var (
 	host       = flag.String("bind-host", "0.0.0.0", "host interface to bind on, defaults to 0.0.0.0")
 	port       = flag.Int("port", 8044, "port to listen on, defaults to 8044")
 )
+var sdnOpts network.SdnOpts
+
+func init() {
+	flag.StringVar(&sdnOpts.EtcdEndpoints, "etcd-endpoints", "http://127.0.0.1:4001,http://127.0.0.1:2379", "a comma-delimited list of etcd endpoints")
+	flag.StringVar(&sdnOpts.EtcdPrefix, "etcd-prefix", "/coreos.com/network", "etcd prefix")
+	flag.StringVar(&sdnOpts.EtcdKeyFile, "etcd-keyfile", "", "SSL key file used to secure etcd communication")
+	flag.StringVar(&sdnOpts.EtcdCertFile, "etcd-certfile", "", "SSL certification file used to secure etcd communication")
+	flag.StringVar(&sdnOpts.EtcdCAFile, "etcd-cafile", "", "SSL Certificate Authority file used to secure etcd communication")
+	flag.StringVar(&sdnOpts.EtcdUsername, "etcd-username", "", "Username for BasicAuth to etcd")
+	flag.StringVar(&sdnOpts.EtcdPassword, "etcd-password", "", "Password for BasicAuth to etcd")
+	flag.StringVar(&sdnOpts.Listen, "listen", "", "run as server and listen on specified address (e.g. ':8080')")
+	flag.StringVar(&sdnOpts.Remote, "remote", "", "run as client and connect to server on specified address (e.g. '10.1.2.3:8080')")
+	flag.StringVar(&sdnOpts.RemoteKeyFile, "remote-keyfile", "", "SSL key file used to secure client/server communication")
+	flag.StringVar(&sdnOpts.RemoteCertFile, "remote-certfile", "", "SSL certification file used to secure client/server communication")
+	flag.StringVar(&sdnOpts.RemoteCAFile, "remote-cafile", "", "SSL Certificate Authority file used to secure client/server communication")
+}
 
 func handleMainErr(err error) {
 	if strings.Compare(os.Getenv("OMALLEY_ENV"), "production") == 0 {
